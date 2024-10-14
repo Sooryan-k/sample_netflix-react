904169c5545474617c28c520df494f339fe07d82
Sooryan K <sooryan@Sooryans-MacBook-Air.local>
2024-10-14 14:24:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
 import Card from './Components/MovieCards/Card';
-import { orginals,action,comedy, horror } from './urls';
+import { orginals,action,comedy, horror, romance, trending } from './urls';
 
 
 
@@ -12,7 +12,8 @@ function App() {
     <div className="App">
       <Navbar/>
       <Banner/>
-      <Card url={orginals} title='Netflix orginals' />
+      <Card url={trending} title='Trending Now' />
+      <Card url={orginals} title='Netflix originals' isSmall/>
       <Card url={action} title='Action' isSmall/>
       <Card url={comedy} title='Comedy' isSmall/>
       <Card url={horror} title='Horror' isSmall/>
--- src/Components/MovieCards/Card.js
+++ src/Components/MovieCards/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Card.css'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from '../../Axios'
-import {imageUrl,API_KEY} from '../../Constants/Constants'
+import { imageUrl, API_KEY } from '../../Constants/Constants'
 import YouTube from 'react-youtube'
 
 function Card(props) {
-    const [card, setCard]= useState([])
-    const [urlId, setUrlId]= useState([])
-    useEffect(()=>{
-        axios.get(props.url).then(response=>{
+    const [card, setCard] = useState([])
+    const [urlId, setUrlId] = useState(null) // Initialize with null to make trailer visible only after click
+    useEffect(() => {
+        axios.get(props.url).then(response => {
             console.log(response.data);
             setCard(response.data.results)
         })
@@ -18,33 +18,37 @@ function Card(props) {
         height: '390',
         width: '100%',
         playerVars: {
-        autoplay: 1,
+            autoplay: 1,
         },
-      };
-      const handleMovieId=(id)=>{
+    };
+    const handleMovieId = (id) => {
         console.log(id);
-        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-            console.log(response.data); 
-            if(response.data.results.length!==0){
+        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+            console.log(response.data);
+            if (response.data.results.length !== 0) {
                 setUrlId(response.data.results[0])
-                
-            }else{
+
+            } else {
                 console.log('array empty');
-                
+                setUrlId(null) // Reset to null if no trailer is available
             }
         })
-      }
-  return (
-    <div className='row'>
-        <h2>{props.title}</h2>
-        <div className="card">
-            {card.map((obj)=>
-            <img onClick={()=>handleMovieId(obj.id)} alt="cards" className={props.isSmall ? 'smallCards':'cards'}  src={`${imageUrl+obj.backdrop_path}`}/>
-            )}  
+    }
+    return (
+        <div className='row'>
+            <h2>{props.title}</h2>
+            <div className="card">
+                {card.map((obj) =>
+                    <img onClick={() => handleMovieId(obj.id)} alt="cards" className={props.isSmall ? 'smallCards' : 'cards'} src={`${imageUrl + obj.backdrop_path}`} />
+                )}
+            </div>
+            {urlId && (
+                <div style={{ paddingLeft: "20px", paddingRight: "20px" }}>
+                    <YouTube opts={opts} videoId={urlId.key} />
+                </div>
+            )} {/* Added equal padding to left and right */}
         </div>
-        {urlId && <YouTube opts={opts} videoId={urlId.key}/>}
-    </div>
-  )
+    )
 }
 
 export default Card
